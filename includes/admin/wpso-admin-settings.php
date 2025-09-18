4399416a02c97a0b466a9c950f882da077d7e63f
srini-ra <137151030+srini-ra@users.noreply.github.com>
2025-09-18 19:59:26 +0530

    Add files via upload

--- includes/admin/wpso-admin-settings.php
+++ includes/admin/wpso-admin-settings.php
@@ -2,40 +2,41 @@
 /**
  * WP Speed Optimization - Admin Settings Page
  * 
- * Renders plugin settings form
+ * Renders plugin settings form including new JS and Image options
  * 
  * @package WP_Speed_Optimization
  * @since 2.0.0
  */
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+if ( ! class_exists( 'WPSO_Admin_Settings' ) ) {
 class WPSO_Admin_Settings {
     private $settings;
 
-    public function __construct($settings) {
+    public function __construct( $settings ) {
         $this->settings = $settings;
     }
 
     public function render() {
         ?>
         <div class="wrap wpso-wrap">
-            <h1><?php esc_html_e('WP Speed Optimization Settings', 'wp-speed-optimization'); ?></h1>
+            <h1><?php esc_html_e( 'WP Speed Optimization Settings', 'wp-speed-optimization' ); ?></h1>
             <form method="post" action="options.php">
                 <?php
-                settings_fields('wpso_settings');
-                do_settings_sections('wpso_settings');
+                settings_fields( 'wpso_settings' );
+                do_settings_sections( 'wpso_settings' );
                 ?>
                 <table class="form-table">
-                    <?php $this->checkbox('cache_enabled', __('Enable Page Cache', 'wp-speed-optimization')); ?>
-                    <?php $this->checkbox('critical_css', __('Generate & Inline Critical CSS', 'wp-speed-optimization')); ?>
-                    <?php $this->checkbox('css_optimization', __('Optimize & Minify CSS', 'wp-speed-optimization')); ?>
-                    <?php $this->checkbox('js_optimization', __('Optimize & Defer JavaScript', 'wp-speed-optimization')); ?>
-                    <?php $this->checkbox('image_optimization', __('Optimize Images & Lazy Load', 'wp-speed-optimization')); ?>
-                    <?php $this->checkbox('database_optimization', __('Enable Database Cleanup', 'wp-speed-optimization')); ?>
-                    <?php $this->checkbox('performance_monitoring', __('Enable Performance Monitoring', 'wp-speed-optimization')); ?>
+                    <?php $this->checkbox( 'cache_enabled', __( 'Enable Page Cache', 'wp-speed-optimization' ) ); ?>
+                    <?php $this->checkbox( 'critical_css', __( 'Generate & Inline Critical CSS', 'wp-speed-optimization' ) ); ?>
+                    <?php $this->checkbox( 'css_optimization', __( 'Optimize & Minify CSS', 'wp-speed-optimization' ) ); ?>
+                    <?php $this->checkbox( 'js_optimization', __( 'Optimize & Defer JavaScript', 'wp-speed-optimization' ) ); ?>
+                    <?php $this->checkbox( 'image_optimization', __( 'Optimize Images & Lazy Load', 'wp-speed-optimization' ) ); ?>
+                    <?php $this->checkbox( 'database_optimization', __( 'Enable Database Cleanup', 'wp-speed-optimization' ) ); ?>
+                    <?php $this->checkbox( 'performance_monitoring', __( 'Enable Performance Monitoring', 'wp-speed-optimization' ) ); ?>
                 </table>
                 <?php submit_button(); ?>
             </form>
@@ -43,13 +44,14 @@ class WPSO_Admin_Settings {
         <?php
     }
 
-    private function checkbox($key, $label) {
-        $checked = !empty($this->settings[$key]) ? 'checked' : '';
+    private function checkbox( $key, $label ) {
+        $checked = ! empty( $this->settings[ $key ] ) ? 'checked' : '';
         ?>
         <tr valign="top">
-            <th scope="row"><?php echo esc_html($label); ?></th>
-            <td><input type="checkbox" name="wpso_settings[<?php echo esc_attr($key); ?>]" <?php echo $checked; ?> /></td>
+            <th scope="row"><?php echo esc_html( $label ); ?></th>
+            <td><input type="checkbox" name="wpso_settings[<?php echo esc_attr( $key ); ?>]" <?php echo $checked; ?> /></td>
         </tr>
         <?php
     }
 }
+} // end if class_exists
